6fd71d92ada58fe8c7f9069e7eeb31f91fd2a9da
wyf <72241996+wyf9@users.noreply.github.com>
2020-11-01 19:35:10 +0800

    outputscII 0.9.9

---
+++ outputSCii.py
@@ -0,0 +1,7 @@
+import datetime
+dateyear=datetime.datetime.now().year
+print("{wyf}Copyright© 2019 -",dateyear," wyf. All Rights Reserved.")
+print("{wyf}不支持多字符!")
+name=input("{wyf}字符=")
+print("{wyf}",name+" 的ASCII码为",ord(name))
+test=input("{wyf}按Enter键退出. . .")
